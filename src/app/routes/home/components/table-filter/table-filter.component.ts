6ab3aab5a40d7a24536ee8abde44baceff2ddc88
rolero4 <72434958+Rolero4@users.noreply.github.com>
2024-09-10 16:57:36 +0200

    element-5: table filter component

--- src/app/routes/home/components/table-filter/table-filter.component.ts
+++ src/app/routes/home/components/table-filter/table-filter.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, output } from "@angular/core";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { debounceTime, filter, Observable, tap } from "rxjs";
 
 @Component({
-  selector: 'app-table-filter',
-  standalone: true,
-  imports: [],
-  templateUrl: './table-filter.component.html',
-  styleUrl: './table-filter.component.scss'
+    selector: "app-table-filter",
+    standalone: true,
+    imports: [
+        MatFormFieldModule,
+        MatInputModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+        MatIconModule,
+    ],
+    templateUrl: "./table-filter.component.html",
+    styleUrl: "./table-filter.component.scss",
 })
 export class TableFilterComponent {
+    private readonly destroyRef = inject(DestroyRef);
 
+    protected readonly filterValueChanged = output<string>();
+    protected readonly filterControl = new FormControl<string>("");
+
+    constructor() {
+        this.filterValueChangeOutput$().subscribe();
+    }
+
+    private filterValueChangeOutput$(): Observable<string> {
+        return this.filterControl.valueChanges.pipe(
+            debounceTime(2000),
+            filter((value): value is string => !!value),
+            tap((value) => this.filterValueChanged.emit(value)),
+            takeUntilDestroyed(this.destroyRef)
+        );
+    }
+
+    protected clearInput(): void {
+        this.filterControl.setValue("");
+    }
 }
--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,35 +1,33 @@
 import { Component, inject } from "@angular/core";
-import { MatDialog } from "@angular/material/dialog";
-import { EditPopupComponent } from "../../shared/components/edit-popup/edit-popup.component";
-import { PeriodicElement } from "../../shared/model/misc.model";
+import { ElementTableComponent } from "./components/element-table/element-table.component";
+import { TableFilterComponent } from "./components/table-filter/table-filter.component";
 import { ElementStoreService } from "./services/element-store.service";
 
 @Component({
     selector: "app-home",
     standalone: true,
-    imports: [],
+    imports: [TableFilterComponent, ElementTableComponent],
     templateUrl: "./home.component.html",
     styleUrl: "./home.component.scss",
 })
 export class HomeComponent {
     private readonly storeService = inject(ElementStoreService);
     protected readonly elements$ = this.storeService.elements$;
+    // constructor(public dialog: MatDialog) {
+    //     this.openEditDialog({
+    //         position: 1,
+    //         name: "kekw",
+    //         weight: 1.2334,
+    //         symbol: "xd",
+    //     });
+    // }
 
-    constructor(public dialog: MatDialog) {
-        this.openEditDialog({
-            position: 1,
-            name: "kekw",
-            weight: 1.2334,
-            symbol: "xd",
-        });
-    }
+    // openEditDialog(element: PeriodicElement): void {
+    //     const dialogRef = this.dialog.open(EditPopupComponent, {
+    //         data: { ...element },
+    //         disableClose: true,
+    //     });
 
-    openEditDialog(element: PeriodicElement): void {
-        const dialogRef = this.dialog.open(EditPopupComponent, {
-            data: { ...element },
-            disableClose: true,
-        });
-
-        dialogRef.afterClosed().subscribe((result) => {});
-    }
+    //     dialogRef.afterClosed().subscribe((result) => {});
+    // }
 }
